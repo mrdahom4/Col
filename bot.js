c876c611e424634985518a3bcc4143c2f4749906
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-01-24 17:14:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -776,8 +776,7 @@ client.on('guildMemberAdd', member => {
         .setThumbnail(m.avatarURL)
         .setAuthor(m.username,m.avatarURL)
         .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
-     
-         .setFooter('https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
+        .setFooter('https://cdn.discordapp.com/attachments/531704526126841856/538013513637953546/Dvbotemoje.png')
      welcomer.send({embed:yumz});          
          
    
